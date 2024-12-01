8089ed402c75c1dff3b14d9711af6e837ca454a0
taesay17 <151002316+taesay17@users.noreply.github.com>
2024-12-01 22:27:46 +0600

    Инициализация списка данных при первом рендере с помощью useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 const DataFilterComponent = () => {
   const [data, setData] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      setData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredData = data.filter(item =>
+    item.name && typeof item.name === 'string' && item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <div>
-      <h1>Data Filter</h1>
-      {/* позже */}
+    <div className="data-filter-container">
+      <input
+        type="text"
+        placeholder="Введите имя для поиска"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="input-search"
+      />
+
+      <ul>
+        {filteredData.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
